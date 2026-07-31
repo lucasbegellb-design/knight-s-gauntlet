a5f22ac513c1cd5e6f47d0dfa03df3366d2db0af
lucasbegellb-design <lucasbegel.lb@gmail.com>
2026-07-31 13:55:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/knight-s-gauntlet/',   
   plugins: [react()],
   test: {
     environment: 'node',
