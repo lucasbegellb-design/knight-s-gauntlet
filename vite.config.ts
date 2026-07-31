e01179d177663f04ec4cbeffb5f907c8d148c186
lucasbegellb-design <lucasbegel.lb@gmail.com>
2026-07-31 15:12:10 +0200

    Move vitest test config into vitest.config.ts and remove 'test' from vite.config.ts to fix TypeScript typing error causing build to fail

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/knight-s-gauntlet/',
+  base: '/knight-s-gauntlet/',   
   plugins: [react()],
 })
